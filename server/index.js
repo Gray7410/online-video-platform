99d7b1dc2d92e79423accba2271e642d493b14fb
Sergey Fomin <gray7410@yandex.ru>
2022-09-20 10:50:07 +0500

    connect to db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
 app.listen(8800, () => {
-  console.log("Connected to server!");
+  connect();
+  console.log("Connected to server");
 });
